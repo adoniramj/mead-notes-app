524d4507f89f47da7bae8441cc662505ccb53eaf
adoniramj <adoniramjvargas@gmail.com>
2020-07-22 15:03:16 -0400

    Completed section 11
    Installed package.json file by using npm init. Downloaded the validator
    package.
    
    Importing third party modules is the same as importing core modules.
    
    Added the nodes_modules folder to .gitignore.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,11 @@
 // const add = require('./utils')
 // console.log(add(2,3))
+const validator = require('validator')
 
 const notes = require('./notes')
 
 const note = notes()
 
 console.log(note)
+
+console.log(validator.isURL('https://mead.io'))
