b4491d5ea1d76872db0d56f1b87723a54e2e1ef3
adoniramj <adoniramjvargas@gmail.com>
2020-07-24 14:14:23 -0400

    Completed section 19
    Added new notes to a JSON file. Verified no duplicates were
    added.
    
    Used try and catch.

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,6 +1,39 @@
-const notes = function(){
+const fs = require('fs')
+
+const getNotes = function(){
     return 'Your notes...'
 }
 
+const addNote = function(title,body){
+    const notes = loadNotes()
 
-module.exports = notes
+    const duplicateNotes = notes.filter(note => {
+        return note.title.toLowerCase() === title.toLowerCase()
+    })
+    
+    console.log(duplicateNotes)
+
+    if(duplicateNotes.length == 0){
+        notes.push({
+            title,
+            body})
+    }
+    saveNotes(notes)
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (error) {
+        return []
+    }
+}
+
+const saveNotes = function(notes) {
+    const notesString = JSON.stringify(notes)
+    fs.writeFileSync('notes.json',notesString)
+}
+
+module.exports = {getNotes, addNote}
