bcef834d31d35ed1e84f0f1d7876631b93798989
adoniramj <adoniramjvargas@gmail.com>
2020-07-24 13:37:11 -0400

    Completed sections 14 -17
    Installed yargs to parse cli inputs.
    
    Review of JSON, parsing and stringify.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,68 @@
-// const add = require('./utils')
-// console.log(add(2,3))
-// const validator = require('validator')
-
-// const notes = require('./notes')
-
-// const note = notes()
-
-// console.log(note)
-
-// console.log(validator.isURL('https://mead.io'))
-
+const yargs = require('yargs')
 const chalk = require('chalk')
 
-console.log(chalk.bgGray.bold('Success!'))
-console.log(chalk.red.inverse('Success'))
-console.log(chalk.blue.inverse('Success...'))
+const notes = require('./notes')
+
+
+// Customize yargs version
+
+yargs.version('1.1.0')
+
+// add, remove, read, list  distinct commands
+
+// Create add command
+
+yargs.command({
+    command: 'add',
+    describe: 'Adds a new note',
+    builder: {
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe : 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Remove a note'
+        }
+    },
+    handler: function() {
+        console.log('Removing a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Shows all notes',
+    handler: function() {
+        console.log('Showing all notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Reads a note',
+    handler : function() {
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse()
+//console.log(yargs.argv)
+//console.log(process.argv)
+
