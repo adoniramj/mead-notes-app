9901e4d382afe7451b79f15ff75748c266f0a93e
adoniramj <adoniramjvargas@gmail.com>
2020-08-18 13:51:24 -0400

    Code refactoring.
    Switch in backend from the request module to axios.
    
    Refactor fron-end and back-end to use Promises.

---
+++ src/Routes/routes.js
@@ -0,0 +1,72 @@
+const coordinates = require('../utils/coodinates')
+const weatherRetrival = require('../utils/weatherRetrival')
+
+
+module.exports = app => {
+  app.get('/', (req, res) => {
+    let time = Date.now()
+    res.render('index', {
+      message: 'Hello',
+      title: 'Weather',
+      name: 'Adoniram',
+      time,
+      info: 'Copyright something',
+    })
+  })
+  
+  app.get('/about', (req, res) => {
+    res.render('about', {
+      title: 'About us',
+      name: 'Adoniram J. Vargas',
+      info: 'Copyright something',
+    })
+  })
+  
+  app.get('/help', (req, res) => {
+    res.render('help', {
+      message: 'Contact Mr. Roboto for assistance!',
+      title: 'Help',
+      name: 'Adoniram',
+      info: 'Copyright something',
+    })
+  })
+  
+  app.get('/weather', (req, res) => {
+    address = req.query.address
+    if (!address) {
+      return res.send({ error: 'Address not provided!' })
+    }
+    coordinates(address, (error, lat, long, place_name) => {
+      if (error) {
+        return res.send({ error })
+      }
+      weatherRetrival(lat, long, (error, temperature, weather_descriptions, winds) => {
+          if (error) {
+            return res.send({ error })
+          }
+          res.send({ lat, long, location: place_name, temperature, weather: weather_descriptions, winds })
+        }
+      )
+    })
+  })
+  
+  app.get('/products', (req, res) => {
+    if (!req.query.search) {
+      return res.send({ error: 'You must provide a search term' })
+    }
+    res.send({ products: [] })
+  })
+  
+  app.get('/help/*', (req, res) => {
+    res.render('404', {
+      message: 'No help',
+    })
+  })
+  
+  app.get('*', (req, res) => {
+    res.render('404', {
+      title: '404',
+      message: 'This page does not exist!',
+    })
+  })
+}
--- src/app.js
+++ src/app.js
@@ -3,20 +3,15 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 
-const geocode = require('./utils/geocode')
-const weather = require('./utils/weather')
-const coordinates = require('./utils/coodinates')
-const weatherRetrival = require('./utils/weatherRetrival')
-const { response } = require('express')
-
 const app = express()
 
 // public directory
-const publicDirectoryPath = path.join(__dirname,'../public')
+console.log(__dirname)
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 // hbs views locations
-const viewsPath = path.join(__dirname,'..','/templates','/views')
-const partialsPath = path.join(__dirname,'..','/templates','/partials')
+const viewsPath = path.join(__dirname, '..', '/templates', '/views')
+const partialsPath = path.join(__dirname, '..', '/templates', '/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
@@ -26,74 +21,9 @@ hbs.registerPartials(partialsPath)
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath)) //searches public folder for a match
 
-app.get('/', (req,res) => {
-	let time = Date.now()
-	res.render('index',{
-		message : 'Hello',
-		title: 'Weather',
-		name: 'Adoniram',
-		time,
-		info : 'Copyright something'
-	})
-})
-
-app.get('/about', (req,res) => {
-	res.render('about', {
-		title: 'About us',
-		name : 'Mr. Robot',
-		info : 'Copyright something'
-	})
-})
-
-app.get('/help', (req,res) => {
-	res.render('help', {
-		message: 'Contact Mr. Roboto for assistance!',
-		title: "Help",
-		name: 'Adoniram',
-		info : 'Copyright something'
-	})
-})
-
-app.get('/weather', (req, res) => { 
-	address = req.query.address
-	if (!address) {
-		return res.send({ error : 'Address not provided!'})
-	}
-	coordinates(address, (error,lat, long, place_name) => {
-		if(error){
-			return res.send({ error })
-		} 
-		weatherRetrival(lat,long, (error, temperature, weather_descriptions, winds) => {
-			if(error){
-				return res.send({ error })
-			}
-			res.send({lat, long, location : place_name, temperature, weather : weather_descriptions, winds})
-		})
-		
-	})
-})
-
-app.get('/products', (req,res) => {
-    if(!req.query.search){
-        return res.send({error : 'You must provide a search term'})
-    }
-    res.send({ products : []})
-})
-
-
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        message : 'No help'
-    })
-})
-
-app.get('*', (req, res) => {
-    res.render('404', {
-        message : 'This page does not exist!'
-    })
-})
+require('./Routes/routes')(app)
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT,() => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
 })
